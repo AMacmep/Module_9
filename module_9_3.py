9c507c6231b77f3cab83c16c6163d5cfe37c7f8a
AMacmep <cherkashinandrey@mail.ru>
2024-10-12 13:23:00 +0700

    12/10

--- module_9_3.py
+++ module_9_3.py
@@ -1,1 +1,9 @@
 # Домашнее задание по теме "Создание потоков".
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+first_result = (len(a) - len(b) for a, b in zip(first, second) if len(a) != len(b))
+
+second_result = (len(first[n]) == len(second[n]) for n in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
