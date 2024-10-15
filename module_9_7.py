07edd3e481310b204427826d418ded520d39d4b9
AMacmep <cherkashinandrey@mail.ru>
2024-10-15 21:24:11 +0700

    12/10

--- module_9_7.py
+++ module_9_7.py
@@ -1,3 +1,26 @@
 # Домашнее задание по теме "Декораторы"
-def sum_three(*dannie):
-    
+def is_prime(func_summ):
+    def wrapper(*args):
+        result=func_summ(*args)
+        #проверка на простое/составное из module_2_4
+        prime = True
+        for N in range(2, result):
+            if result == 1:
+                continue
+            if result % N == 0:
+                prime = False
+                break
+        if prime:
+            print('Простое')
+        else:
+            print('Составное')
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(*args):
+    result = sum(args)
+    return result
+
+result = sum_three(2, 3, 6)
+print(result)
